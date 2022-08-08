ca2dc40fde808d49b9f81269a69de8ec5105eb4d
roshnipatell <patelroshni9045@gmail.com>
2022-08-08 08:17:24 +0530

    P1_1

---
+++ src/main/kotlin/practical-1.kt
@@ -0,0 +1,33 @@
+fun main(){
+    var x : Int=100
+    var y: Float=200F
+    var a: Char='K'
+    val str = "Keyur"
+    var b:Boolean=false
+    var m: Double=23.46577
+    var n: Long=237654
+    var l: Short=-5
+    var k: Byte=-123
+
+
+
+    println("Integer value:$x")
+    println("Float value:$y")
+    println("Character value:$a")
+    println("String value:$str")
+    println("Boolean value:$b")
+    println("Double value:$m")
+    println("Long value:$n")
+    println("Short value:$l")
+    println("Byte value:$k")
+
+
+
+
+
+
+
+
+
+
+}
