7848460cf49ab47c70f17039cb45a963164bf7e8
roshnipatell <patelroshni9045@gmail.com>
2022-08-08 08:50:14 +0530

    P1_2

---
+++ src/main/kotlin/practical-2.kt
@@ -0,0 +1,19 @@
+fun main(){
+    var x : Int=100
+    val y:Double=x.toDouble()
+    var value:String="10"
+    var value1:Int=value.toInt()
+    var value2:Int=value.toInt()
+    var dub:Double=value.toDouble()
+
+
+    println("Integer Value:$x")
+    println("Double value (From Integer):$y")
+    println("Integer value1 (From String):$value1")
+    println("Integer value1 (From String):$value1")
+    println("Integer value2 (From String):$value2")
+    println("Double value (From String):$dub")
+
+
+
+}
