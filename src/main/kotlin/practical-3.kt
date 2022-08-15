3d42e45ac5348c7ce0e4d5ca73cefc2648476a7b
Keyur Raval <ravalkeyur054@gmail.com>
2022-08-16 00:17:11 +0530

    Practical-1_3

---
+++ src/main/kotlin/practical-3.kt
@@ -0,0 +1,33 @@
+fun main(){
+    print("Student Enrollment no:")
+    var str1 = readLine()!!.toLong()
+    print("Student Name:")
+    var str2 = readLine()!!.toString()
+    print("Student Branch:")
+    var str3 = readLine()!!.toString()
+    print("Student Class:")
+    var str4 = readLine()!!.toString()
+    print("Student Batch:")
+    var str5 = readLine()!!.toString()
+    print("Student College Name:")
+    var str6 = readLine()!!.toString()
+    print("Student University Name:")
+    var str7 = readLine()!!.toString()
+    print("Student Age:")
+    var str8 = readLine()!!.toInt()
+
+    println("Enrollment No: = $str1")
+    println("Name: = $str2")
+    println("Age: = $str8")
+    println("Branch: = $str3")
+    println("Class: = $str4")
+    println("Batch: = $str5")
+    println("College Name: = $str6")
+    println("University Name: = $str7")
+
+
+
+
+
+
+}
