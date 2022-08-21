99d1b249c7b2d9b80190b3540e81a39bb0dbafda
Keyur Raval <ravalkeyur054@gmail.com>
2022-08-21 23:16:28 +0530

    Practical-1_4

---
+++ src/main/kotlin/practical-4.kt
@@ -0,0 +1,11 @@
+fun main(){
+    print("Enter the number:")
+    var num=readLine()!!.toInt()
+
+    if(num%2==0){
+        print("$num is even.")
+    }else{
+        print("$num is odd.")
+
+    }
+}
